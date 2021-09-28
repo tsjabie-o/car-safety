42292bf7222dbfac9ff09d75f65c8d6f5de49f51
Xavi Oorthuis <x.e.oorthuis@gmail.com>
2021-09-28 17:02:46 +0200

    functional method of converting input to list of vectors

--- functions.py
+++ functions.py
@@ -3,5 +3,53 @@ def ExtractData():
 
     with open("./data/car.data") as data:
         for line in data:
-            features = line.split(",")
+            features = line.strip().split(",")
+            for i in range(len(features)):
+                features[i] = ToNumerical(features[i], i)
+            cars.append(features)
+    
+    return cars
             
+def ToNumerical(text, i):
+    database = {
+        0:{
+            "low":0,
+            "med":1,
+            "high": 2,
+            "vhigh": 3
+        },
+        1:{
+            "low":0,
+            "med":1,
+            "high": 2,
+            "vhigh": 3
+        },
+        2:{
+            "2":0,
+            "3":1,
+            "4":2,
+            "5more":3
+        },
+        3:{
+            "2":0,
+            "4":1,
+            "more":2
+        },
+        4:{
+            "small":0,
+            "med":1,
+            "big":2
+        },
+        5:{
+            "low":0,
+            "med":1,
+            "high": 2,
+        },
+        6:{
+            "unacc":0,
+            "acc":1,
+            "good":2,
+            "vgood":3
+        }
+    }
+    return database[i][text]
